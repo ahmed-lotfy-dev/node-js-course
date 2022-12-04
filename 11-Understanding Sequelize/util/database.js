6f248a5ebf8250e6b1ff135acfead07c8fd816bf
ahmed-lotfy-dev <elshenawy19@gmail.com>
2022-12-04 17:16:32 +0200

    Finishing Section 11 Understanding Sequelize

---
+++ 11-Understanding Sequelize/util/database.js
@@ -0,0 +1,6 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('node-db', 'root', process.env.DB_PASSWORD,
+  { dialect: 'mysql', host: 'localhost' })
+
+module.exports = sequelize
