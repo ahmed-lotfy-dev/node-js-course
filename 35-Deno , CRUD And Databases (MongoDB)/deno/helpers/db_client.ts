021daef3c0a2f39d7e52fdd2513c02cee1b1e603
ahmed-lotfy-dev <elshenawy19@gmail.com>
2022-12-22 12:26:28 +0200

    Finishing Section 35-Deno,Crud And Databases (MongoDB) fixing api leaked for the db CHANGED USER API KEY

--- 35-Deno , CRUD And Databases (MongoDB)/deno/helpers/db_client.ts
+++ 35-Deno , CRUD And Databases (MongoDB)/deno/helpers/db_client.ts
@@ -8,6 +8,8 @@ const client = new MongoClient();
 
 export async function connect() {
   await client.connect(
+    // Changed and deleted the user Dont BOTHER
+    // Create your own one from MONGO DB ATLAS ITS FREE
     "mongodb+srv://node-db:yJulykHuRjvv6RJV@todo-app.quv8u2u.mongodb.net/todo-app?authMechanism=SCRAM-SHA-1"
   );
   console.log("Database connection was successful!");
